38d2e8167385a9ef06f97750ef18dfeba2194037
SterlingB9 <SBoelter727@gmail.com>
2024-10-12 22:22:43 -0600

    Lesson 2

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+const contactsFilePath = path.join(__dirname, '../contacts.json');
+
 const awesomeFunction = (req, res, next) => {
     res.json('Lindsey Boelter');
 };
@@ -6,4 +11,40 @@ const returnAnotherPerson = (req, res, next) => {
     res.json('Super awesome person');
 };
 
-module.exports = { awesomeFunction, returnAnotherPerson };
+const getAllContacts = (req, res, next) => {
+    fs.readFile(contactsFilePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: 'Unable to read contacts file' });
+        }
+        // Parse the JSON data from the file
+        const contacts = JSON.parse(data);
+
+        // Extract names of all contacts
+        const allNames = Object.values(contacts[0]).map(contact => `${contact.firstName} ${contact.lastName}`);
+
+        // Send the list of names as a JSON response
+        res.json(allNames);
+    });
+};
+
+const getContactByFirstName = (req, res, next) => {
+    const { firstName } = req.params;
+
+    fs.readFile(contactsFilePath, 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ message: 'Error reading contacts file' });
+        }
+        const contacts = JSON.parse(data);
+
+        // Loop through the contacts and find the one that matches the firstName
+        const contact = Object.values(contacts[0]).find(c => c.firstName === firstName);
+
+        if (contact) {
+            res.json(contact);
+        } else {
+            res.status(404).json({ message: 'Contact not found' });
+        }
+    });
+};
+
+module.exports = { awesomeFunction, returnAnotherPerson, getAllContacts, getContactByFirstName };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const routes = require('express').Router();
-
 const myController = require('../controllers');
+const path = require('path');
 
 routes.get('/', myController.awesomeFunction);
 routes.get('/awesome', myController.returnAnotherPerson);
+routes.get('/contacts', myController.getAllContacts);
+routes.get('/contacts/:firstName', myController.getContactByFirstName);
 
 module.exports = routes;
